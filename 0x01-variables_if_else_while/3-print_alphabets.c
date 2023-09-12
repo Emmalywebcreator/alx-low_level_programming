cd4cb4f057a418de1eed3f3dee0a224992af2eea
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-09-11 18:04:35 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print alphabet in lowercase then
+ * in upper case
+ *
+ * Return: 0 always for success
+ */
+
+int main(void)
+{
+	char alphabets = 'a';
+	char ALPHABET = 'A';
+
+	while (alphabets <= 'z')
+	{
+		putchar(alphabets);
+		alphabets++;
+	}
+
+	while (ALPHABET <= 'Z')
+	{
+		putchar(ALPHABET);
+		ALPHABET++;
+	}
+	putchar('\n');
+	return(0);
+}
