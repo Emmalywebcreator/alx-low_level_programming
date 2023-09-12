b87c3c112d0cb1718b72073da80fe3a5d0537160
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-09-11 18:53:20 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print alphabets in lowecase except 'e' and 'q'
+ *
+ * Return: 0 always for success
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+			{
+				if (alphabet == 'e' || alphabet == 'q')
+				continue;
+			}
+	
+		putchar(alphabet);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
