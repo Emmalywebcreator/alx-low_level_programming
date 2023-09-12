71a4abd55c06c65c718447edd3633d84a74143ef
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-09-12 13:13:47 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program print putchar
+ * follow by a new line
+ *
+ * Return: 0 always for success
+ */
+
+int main()
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+
+	return (0);
+}
+
