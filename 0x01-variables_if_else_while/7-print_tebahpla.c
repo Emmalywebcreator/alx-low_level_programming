38db6c275e7955590d58d5d29688f866d411c1d4
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-09-11 20:36:27 -0700

    7-print_teba

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prinnt all alphabet in lower case followed by a new
+ * line
+ *
+ * Return: 0 always for success
+ */
+
+int main(void)
+{
+        char alphabets = 'z';
+
+        while (alphabets >= 'a')
+        {
+        putchar(alphabets);
+        alphabets--;
+        }
+
+        putchar('\n');
+
+        return (0);
+}
