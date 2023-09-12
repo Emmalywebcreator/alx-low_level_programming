3035f84057f5bc8462c35605ca8d7133259cbf0f
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-09-12 14:29:13 -0700

    print alphabet in lower case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: Print the alphabet in lowercase
+ * Description: print_alphabet in lowercase
  * followed by anew line
  *
  * Return: 0 always for success
