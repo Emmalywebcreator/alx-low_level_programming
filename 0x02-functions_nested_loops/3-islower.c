76e753ddd22df7b599f6c0cb83fa18bba9ac8389
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-09-12 17:40:29 -0700

    3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islowwer - check wheater char is a lowercase
+ *
+ * Return: 0 or 1
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (0);
+	}
+	
+	else
+	{
+		return (0);
+	}
+}
+	
