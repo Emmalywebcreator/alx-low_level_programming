a2ecfb6650e27915c3ffad316d45902bb780780a
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-09-14 20:14:17 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ * return: void
+ */
+
+void print_numbers(void)
+{
+	char num;
+
+	num = '0';
+	while (num <= '9')
+	{
+		_putchar(num);
+	       num++;
+	}
+
+	_putchar('\n');
+}	
