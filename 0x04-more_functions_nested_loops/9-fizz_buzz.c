8d2181d9c7cdbdf7b7268e5a8b2d56f3c4ea2e8e
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-09-15 19:16:11 -0700

    triangle

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - prints the numbers from 1 to 100, followed by a new line
+ * but for multiples of three prints Fizz instead of the number
+ * and for the multiples of five prints Buzz
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int k;
+
+	for(k = 1; k <= 100; k++)
+	{
+		if (k % 5 !=0 && k % 3 ==0)
+		{
+			printf("Fizz");
+		}
+		else if (k % 3 != 0 && k % 5 ==0)
+		{
+			printf("Buzz");
+
+		}
+		else if (k % 3 == 0 && k % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+
+		else if (k == 1)
+		{
+			printf("%d", k);
+		}
+
+		else
+		{
+			printf(" ");
+		}
+			return (0);
+			
+			printf('\n');
+	}
+}		
