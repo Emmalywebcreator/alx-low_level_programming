8b710e31a3b49812d222a188bd1757029bc4c91e
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-09-18 04:44:46 -0700

    reseting n from 402 to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - A function that takes a pointer ti ab unt as a para
+ * meter and updates the value it point to to 98
+ *
+ * @n: input variable
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
