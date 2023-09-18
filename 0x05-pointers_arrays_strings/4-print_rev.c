97d3e7754da49166dcabb0f078243319225e9d35
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-09-18 14:41:54 -0700

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+	int rev;
+
+	while(*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	s--;
+	for (rev = length; rev > 0; rev--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
