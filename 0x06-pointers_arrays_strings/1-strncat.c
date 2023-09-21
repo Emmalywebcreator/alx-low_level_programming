b3d2a648f05bc915998b767299239aa773f7665b
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-09-20 20:39:33 -0700

    Concatenation of two strings using at most n byts from the source code

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenate two strings using at most 
+ * n bytes from source string
+ *
+ * @dest: destination string
+ * @src: source srting
+ * @n numbeer of byte from the source string
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int s1;
+	int s2;
+
+	s1 = 0;
+	while (dest[s1] != '\0')
+		s1++;
+
+	s2 = 0;
+	while (src[s2] != '\0' && s2 < n)
+	{
+		dest[s1] = src[s2];
+		s1++;
+		s2++;
+	}
+
+	dest[s1] = '\0';
+	return (dest);
+}
