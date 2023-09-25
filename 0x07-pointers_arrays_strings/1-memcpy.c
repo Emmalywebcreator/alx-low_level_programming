3cb836857721df4aedfb908506d3155382926a3f
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-09-25 15:16:03 -0700

    This programe copies n byte from the memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - Copies n bytes from  memory area
+ * @src: Source from which memory is being copied
+ * @dest: Destination where memory is copied to
+ * @n: Number of byte to be copied
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+		return (dest);
+}
