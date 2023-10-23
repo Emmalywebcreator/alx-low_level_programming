b9c802dd3920806b69bdd8cc54f4bbdbf0c94e0f
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-10-23 08:51:40 -0700

    printint element in a linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,7 +15,7 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 		printf("[0] (nil)\n");
-		
+
 		else
 		printf("[%u] %s\n", h->len, h->str);
 
@@ -23,5 +23,5 @@ size_t print_list(const list_t *h)
 		count++;
 	}
 
-	return count;
+	return (count);
 }
