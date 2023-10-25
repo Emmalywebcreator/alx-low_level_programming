1f516f34f5da277c50e38b4af6ae43c90ca9d57f
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-10-25 02:38:00 -0700

    find loop in a linked list

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -13,27 +13,24 @@ listint_t *find_listint_loop(listint_t *head)
 
 	while (tortoise != NULL && here != NULL && here->next != NULL)
 	{
-		/* Move the tortoise one step */
-		tortoise = tortoise->next;
-		/* Move the hare two steps */
-		here = here->next->next;
 
-		/* If they meet, there's a loop */
+		tortoise = tortoise->next;
+
+		here = here->next->next;
 		if (tortoise == here)
 		{
-			/* Reset one pointer to the head */
+
 			tortoise = here;
 
-			/* Move them at the same speed until they meet */
+
 			while (tortoise != here)
 			{
 				tortoise = tortoise->next;
 				here = here->next;
 			}
-			/* The node where the loop starts */
+
 			return (tortoise);
 		}
 	}
-	/* No loop found */
 	return (NULL);
 }
