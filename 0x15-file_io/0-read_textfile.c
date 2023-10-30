a44d0618e99a09e8b74b4b505b9d4b8f3568e5cf
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-10-30 14:32:56 -0700

    This program read a file and print it to the posix standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * read _textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to be read and print
+ *
+ * Return: the number of letters printed, or 0 if it failed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	int read_result, write_result;
+	char *buf;
+
+	if(!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+	{
+		close(fd);
+		return (0);
+	}
+
+	read_result = read(fd, buf, letters);
+	if (read_result < 0)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	buf[read_result] = '\0';
+
+	close(fd);
+
+	write_result = write(STDOUT_FILENO, buf, read_result);
+	if (write_result < 0)
+	{
+		free(buf);
+		return (0);
+	}
+
+	free(buf);
+	return (write_result);
+}
+
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,10 @@
 #define _MAIN_H_
 
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h> 
+#include <fcntl.h>
+#include <unistd.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
