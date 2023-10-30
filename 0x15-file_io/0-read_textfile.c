f93a2292294214717130f8e643c3628fe932699e
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-10-30 14:44:03 -0700

    hanling betty error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int read_result, write_result;
 	char *buf;
 
-	if(!filename)
+	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
