5c226792ec96bec24743b8c6efdc8821beb95504
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-10-31 16:32:40 -0700

    updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,21 +14,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int read_result, write_result;
 	char *buf;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	if (fd == -1)
 		return (0);
 
 	buf = malloc(sizeof(char) * letters);
-	if (!buf)
+	if (buf == NULL)
 	{
 		return (0);
 	}
 
 	read_result = read(fd, buf, letters);
-	if (read_result < 0)
+	if (read_result == -1)
 	{
 		free(buf);
 		return (0);
@@ -38,7 +38,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	close(fd);
 
 	write_result = write(STDOUT_FILENO, buf, read_result);
-	if (write_result < 0)
+	if (write_result == -1 || write_result != read_result)
 	{
 		free(buf);
 		return (0);
