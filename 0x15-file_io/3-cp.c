46b134af015bdea1997306fd361f12856e074ae3
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-11-02 14:46:36 -0700

    this program copies the content of one file to anothe

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	int fd_r, fd_w, r, a, b;
+	int file_from, file_to, r, a, b;
 
 	char buf[BUFSIZ];
 
@@ -19,35 +19,47 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd_r = open(argv[1], O_RDONLY);
-	if (fd_r < 0)
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((r = read(fd_r, buf, BUFSIZ)) > 0)
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	
+	if (file_to < 0)
 	{
-		if (fd_w < 0 || write(fd_w, buf, r) != r)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			
+		exit(99);
+	}
+	
+	while ((r = read(file_from, buf, BUFSIZ)) > 0)
+	{
+		if (write(file_to, buf, r) != r)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(fd_r);
-			exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
 		}
 	}
-	if (r < )
+
+	if (r < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
 		exit(98);
 	}
-	a = close(fd_r);
-	b = close(fd_w);
-	if (a < 0 || b < 0)
+
+	a = close(file_to);
+	b = close(file_from);
+
+	if (a < 0)
 	{
-		if (a < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
-		if (b < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
+	if (b < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 	return (0);
