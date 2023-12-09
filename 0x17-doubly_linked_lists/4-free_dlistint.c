03367eb350e8b027dcf5a775e1ffb9c1f6d46d38
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-12-08 18:30:25 -0800

    4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,21 +1,18 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * free_dlistint - Free list
- *
- * @head: struct
- *
- * Return: void
+ * free_dlistint - frees a dlistint_t list
+ * @head: pointer to the head of the doubly linked list
  */
-
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *list;
+	dlistint_t *temp;
 
-	while (head)
+	while (head != NULL)
 	{
-		list = head;
-		head = head->next;
-		free(list);
+		temp = head;          /* Save the current node */
+		head = head->next;    /* Move to the next node */
+ 		free(temp);           /* Free the current node */
 	}
 }
