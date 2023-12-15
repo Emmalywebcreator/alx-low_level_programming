e863c16aab9a90648a40603e6b5e9dee6e120b28
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-12-15 15:55:49 -0800

    code injection

---
+++ 0x18-dynamic_libraries/dynacal.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - add two intergers
+ * @a: first integer
+ * @b: second integer
+ * Return: the addition of a and b
+ */
+
+int add(int a, int b)
+{
+	return(a + b);
+}
+
+/**
+ * sub - sub two intergers
+ * @a: first integer
+ * @b: second integer
+ * Return: the difference between a and b
+ */
+
+int sub(int a, int b)
+{
+	return(a - b);
+}
+
+/**
+ * mul - multiply two intergers
+ * @a: first integer
+ * @b: second integer
+ * Return: the product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return(a * b);
+}
+
+/**
+ * div - divide two intergers
+ * @a: first integer
+ * @b: second integer
+ * Return: the division of a and b
+ */
+
+int div(int a, int b)
+{
+	return(a / b);
+}
+
+/**
+ * mod - modulus two intergers
+ * @a: first integer
+ * @b: second integer
+ * Return: the modulus of a and b
+ */
+
+int mod(int a, int b)
+{
+	return(a % b);
+}
